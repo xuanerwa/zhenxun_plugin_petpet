4e665a77c3919f58bef6a7588a0aa43bef39ec1b
yajiwa <839790708@qq.com>
2022-05-31 20:37:17 +0800

    feat: supported zhenxun

---
+++ README.md
@@ -0,0 +1,29 @@
+# zhenxun_plugin_petpet
+
+zhenxun_bot 头像表情包插件
+
+移植自[nonebot-plugin-petpet](https://github.com/noneplugin/nonebot-plugin-petpet)
+
+## 插件安装
+
+1. 将本插件文件放到自定义插件目录下，放到zhenxun_bot/plugins下也行(不推荐，更新会替换这个目录)
+
+2. 安装相关依赖
+
+   ```
+   pip3 install -r requirements.txt
+   ```
+
+### 使用
+
+- 指令 + @user，如： 爬 @小Q
+- 指令 + qq号，如：爬 123456
+- 指令 + 自己，如：爬 自己
+- 指令 + 图片，如：爬 [图片]
+
+### 更新
+
+**2022/5/31**[v0.1]
+
+1. 对真寻进行了适配
+
---
+++ __init__.py
@@ -0,0 +1,71 @@
+from io import BytesIO
+from typing import Union
+from nonebot.params import Depends
+from nonebot.matcher import Matcher
+from nonebot.typing import T_Handler
+from nonebot import on_command, require
+from nonebot.adapters.onebot.v11 import MessageSegment
+from configs.path_config import IMAGE_PATH
+
+require("nonebot_plugin_imageutils")
+
+from .depends import split_msg
+from .data_source import commands
+from .utils import Command, help_image
+
+__zx_plugin_name__ = "头像表情包"
+__plugin_usage__ = """
+usage：
+    触发方式：指令 + @user/qq/自己/图片
+    发送“头像表情包”查看支持的指令
+    指令：
+        摸 @任何人
+        摸 qq号
+        摸 自己
+        摸 [图片]
+""".strip()
+__plugin_des__ = "生成各种表情"
+__plugin_type__ = ("群内小游戏",)
+__plugin_cmd__ = ["头像表情包", "头像相关表情包", "头像相关表情制作"]
+__plugin_version__ = 0.1
+__plugin_author__ = "MeetWq"
+__plugin_settings__ = {
+    "level": 5,
+    "default_status": True,
+    "limit_superuser": False,
+    'cmd': __plugin_cmd__
+}
+__plugin_resources__ = {
+    "images": IMAGE_PATH / "petpet", }
+
+help_cmd = on_command("头像表情包", aliases={"头像相关表情包", "头像相关表情制作"}, block=True, priority=5)
+
+
+@help_cmd.handle()
+async def _():
+    img = await help_image(commands)
+    if img:
+        await help_cmd.finish(MessageSegment.image(img))
+
+
+def create_matchers():
+    def handler(command: Command) -> T_Handler:
+        async def handle(
+                matcher: Matcher, res: Union[str, BytesIO] = Depends(command.func)
+        ):
+            if isinstance(res, str):
+                await matcher.finish(res)
+            await matcher.finish(MessageSegment.image(res))
+
+        return handle
+
+    for command in commands:
+        on_command(
+            command.keywords[0],
+            aliases=set(command.keywords),
+            block=True,
+            priority=5,
+        ).append_handler(handler(command), parameterless=[split_msg()])
+
+
+create_matchers()
